8d650864c24cd9da2dda4b44b491a84db447b2e8
lisabeihypacheco <97892537+lisabeihypacheco@users.noreply.github.com>
2025-02-13 11:13:12 +0000

    Deploying to gh-pages from @ lisabeihypacheco/lisabeihypacheco.github.io@44bf40f2d44ebf649cbbe00553e7df358885ea73 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,10 +25,10 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/assets/pdf/CV_LisaPacheco.pdf";
           },
         },{id: "nav-teaching",
           title: "teaching",
