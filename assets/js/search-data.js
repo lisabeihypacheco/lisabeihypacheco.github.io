560f6ede2dd30858674353cd10f787d0ee3bee15
lisabeihypacheco <97892537+lisabeihypacheco@users.noreply.github.com>
2025-02-17 13:35:37 +0000

    Deploying to gh-pages from @ lisabeihypacheco/lisabeihypacheco.github.io@9c97b829975827df962bc82ea2da7decfde2c4d1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,12 +16,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+        },{id: "nav-eee",
+          title: "eee",
+          description: "Economics of Energy and Environment Programme",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/eee/";
           },
         },{id: "nav-cv",
           title: "cv",
